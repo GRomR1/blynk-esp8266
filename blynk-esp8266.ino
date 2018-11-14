e1a9b62dc422b7cc40f6b3c9aa1d0aab85ce6d2b
Ruslan Gainanov <rgaynanov@nas.company>
2018-11-14 17:55:13 +0500

    Fix

--- blynk-esp8266.ino
+++ blynk-esp8266.ino
@@ -8,17 +8,15 @@
 #include <OneWire.h> // необходимые библиотеки для работы с датчиком температуры
 #include <DallasTemperature.h> // необходимые библиотеки для работы с датчиком температуры
 
-// сигнальный провод датчика температуры подключен к 2 пину на Arduino
-#define ONE_WIRE_BUS 2
-
 // реле с лампочкой подключено к пину 1
 #define RELAY_PIN 1
 
+// сигнальный провод датчика температуры подключен к 2 пину на Arduino
+#define ONE_WIRE_BUS 2
 // настроим библиотеку 1-Wire для связи с датчиком
 OneWire oneWire(ONE_WIRE_BUS);
-
 // создадим объект для работы с библиотекой DallasTemperature
-DallasTemperature sensors(&oneWire);
+DallasTemperature sensorTemp(&oneWire);
 
 // создаём объект для работы с датчиком освещённости
 // и передаём ему номер пина выходного сигнала
@@ -49,7 +47,7 @@ void sendSensorsData()
 {
   // This function sends Arduino up time every 1 second to Virtual Pin (V5)
   // In the app, Widget's reading frequency should be set to PUSH
-  // You can send anything with any interval using this construction
+  // You can send nything with any interval using this construction
   // Don't send more that 10 values per second
 
   // считывание данных с датчика освещённости
@@ -63,14 +61,14 @@ void sendSensorsData()
   Blynk.virtualWrite(V1, sensorLight.getLightLux());
 
   // отправляем запрос на измерение температуры
-  sensors.requestTemperatures();
+  sensorTemp.requestTemperatures();
   // покажем температуру в мониторе Serial порта
   Serial.print("Temp: ");
-  Serial.print(sensors.getTempCByIndex(0));
+  Serial.print(sensorTemp.getTempCByIndex(0));
   Serial.print(" C\n");
 
   // отправка данных сенсора на пин V2
-  Blynk.virtualWrite(V2, sensors.getTempCByIndex(0));
+  Blynk.virtualWrite(V2, sensorTemp.getTempCByIndex(0));
 }
 
 void loop()
